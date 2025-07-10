07c1fb6d09d755319f0a17e3dc8c43261f30ff8d
twinstackstudio <twinstackstudio@gmail.com>
2025-07-10 15:13:56 +0000

    remove the weird logo from across the app

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,14 +1,5 @@
+import { BrainCircuit } from 'lucide-react';
+
 export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg
-      viewBox="0 0 32 32"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      {...props}
-    >
-        <path
-            fill="currentColor"
-            d="M21.218 19.34L17.5 14.25V5h-3v9.25L10.78 19.34l-2.06-1.21 6.28 10.87 6.28-10.87-2.062 1.21zM16 31l-8-13.86 2.06-.03.44.73 5.5-9.52V5h-5V3h12v2h-5v3.34l5.5 9.52.44-.73 2.06.03L16 31z"
-        ></path>
-    </svg>
-  );
-  
+  <BrainCircuit {...props} />
+);
