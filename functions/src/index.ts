2c0b7eedd152673adce077d8f95b23a08c8274e8
twinstackstudio <twinstackstudio@gmail.com>
2025-07-12 17:13:22 +0000

    deploy the firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,14 @@
 "use server";
 
 import {onCallGenkit} from "firebase-functions/v2/https";
-import {chat as chatFlow} from "../../src/ai/flows/chat-flow";
+import {chatFlow} from "../../src/ai/flows/chat-flow";
 
-export const chat = onCallGenkit({
-  authPolicy: (auth) => !!auth,
-}, chatFlow);
+// This will be deployed as a callable function named 'chat'.
+export const chat = onCallGenkit(
+  {
+    // You can configure auth policies here.
+    // For example, to require authentication:
+    // authPolicy: (auth) => !!auth,
+  },
+  chatFlow
+);
