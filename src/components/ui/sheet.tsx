930005e64f77affadbce06e4f2d3ad4b0f3b0662
twinstackstudio <twinstackstudio@gmail.com>
2025-07-24 07:11:39 +0000

    that change broke the buttons, i cant open the sidebars anymore

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -27,7 +27,7 @@ const SheetOverlay = React.forwardRef<
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 0.3 }}
+      transition={{ duration: 0.3, ease: 'easeInOut' }}
       className={cn("fixed inset-0 z-50 bg-black/80", className)}
       {...props}
     />
